e42cb03ef6295e5b4383ddefdb2e1f7aac4fc132
Arnie <arnie752@gmail.com>
2023-09-30 21:19:58 +0300

    Question 5 Complete: Repeating-Key XOR

---
+++ cryptopals_set_1/repeating_key_xor.py
@@ -0,0 +1,21 @@
+def repeated_key_xor(plain_text, key):
+    # returns plain text by repeatedly xor-ing it with key
+    pt = plain_text
+    len_key = len(key)
+    encoded = []
+
+    for i in range(0, len(pt)):
+        encoded.append(pt[i] ^ key[i % len_key])
+    return bytes(encoded)
+
+
+def main():
+    plain_text = b'Burning \'em, if you ain\'t quick and nimble\nI go crazy when I hear a cymbal'
+    key = b'ICE'
+
+    print("Plain text: ", plain_text)
+    print("Encrypted as: ", repeated_key_xor(plain_text, key).hex())
+
+
+if __name__ == '__main__':
+    main()
